d1274f0b2d0f565a0fec39f43fc9f097237e3484
threelittlemonkeys <sjlee7642@gmail.com>
2018-07-23 16:54:14 +0900

    Update model.py

--- transformer/model.py
+++ transformer/model.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -21,6 +22,7 @@ SOS_IDX = 2
 
 torch.manual_seed(1)
 CUDA = torch.cuda.is_available()
+CUDA = False
 
 class transformer(nn.Module):
     def __init__(self, vocab_size):
@@ -31,8 +33,8 @@ class transformer(nn.Module):
         if CUDA:
             self = self.cuda()
 
-    def forward(self, x, mask):
-        self.encoder(x)
+    def forward(self, x, mask = None):
+        self.encoder(x, mask)
 
 class encoder(nn.Module):
     def __init__(self, vocab_size):
@@ -40,12 +42,12 @@ class encoder(nn.Module):
 
         # architecture
         self.embed = nn.Embedding(vocab_size, EMBED_SIZE, padding_idx = PAD_IDX)
-        self.layers = nn.Sequential(*[enc_layer(vocab_size) for _ in range(NUM_LAYERS)])
+        self.layers = nn.ModuleList([enc_layer(vocab_size) for _ in range(NUM_LAYERS)])
 
-    def forward(self, x):
+    def forward(self, x, mask):
         x = self.embed(x)
-        h = self.layers(x)
-        exit()
+        for layer in self.layers:
+            x = layer(x, mask)
 
 class enc_layer(nn.Module): # encoder layer
     def __init__(self, vocab_size):
@@ -54,28 +56,33 @@ class enc_layer(nn.Module): # encoder layer
         # architecture
         self.attn = attn(vocab_size)
 
-    def forward(self, x):
-        self.attn(x)
-        return x
+    def forward(self, x, mask):
+        self.attn(x, mask)
 
 class attn(nn.Module): # multi-head attention
     def __init__(self, vocab_size):
         super().__init__()
         self.h = 8 # number of heads
-        self.d = EMBED_SIZE // self.h # head dimension
+        self.d = EMBED_SIZE // self.h # dimension of each head
 
         # architecture
-        self.Wq = nn.Linear(EMBED_SIZE, self.h * self.d)
-        self.Wk = nn.Linear(EMBED_SIZE, self.h * self.d)
-        self.Wv = nn.Linear(EMBED_SIZE, self.h * self.d)
+        self.Wq = nn.Parameter(Tensor(self.h, EMBED_SIZE, self.d))
+        self.Wk = nn.Parameter(Tensor(self.h, EMBED_SIZE, self.d))
+        self.Wv = nn.Parameter(Tensor(self.h, EMBED_SIZE, self.d))
 
-    def forward(self, x):
-        print("x", x.size())
-        q = self.Wq(x).view(BATCH_SIZE * self.h, -1, self.d) # queries
-        k = self.Wk(x).view(BATCH_SIZE * self.h, -1, self.d) # keys
-        v = self.Wv(x).view(BATCH_SIZE * self.h, -1, self.d) # values
-        score = q.bmm(k.transpose(1, 2))
-        return x
+    def forward(self, x, mask):
+        h = x.unsqueeze(1)
+        q = torch.matmul(h, self.Wq) # query
+        k = torch.matmul(h, self.Wq) # key 
+        v = torch.matmul(h, self.Wq) # value
+        # scaled dot-product attention
+        score = torch.matmul(q, k.transpose(2, 3)) / math.sqrt(self.d)
+        # mask
+        score = F.softmax(score, 2)
+        score = torch.matmul(score, v)
+        score = score.transpose(1, 2).contiguous().view(x.size())
+        print(score.size())
+        exit()
 
 def Tensor(*args):
     x = torch.Tensor(*args)
