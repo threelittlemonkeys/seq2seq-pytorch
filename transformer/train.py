4cbbab240b2532ff8e67dc7102179c2bbcd57558
threelittlemonkeys <sjlee7642@gmail.com>
2018-08-04 17:48:15 +0900

    Update by commit

--- transformer/train.py
+++ transformer/train.py
@@ -72,11 +72,12 @@ def train():
             dec_in = LongTensor([SOS_IDX] * BATCH_SIZE).unsqueeze(1)
             for t in range(y.size(1)):
                 dec_out = dec(enc_out, dec_in, mask)
-                loss += F.nll_loss(dec_out, y[:, t], ignore_index = PAD_IDX)
+                loss += F.nll_loss(dec_out, y[:, t], size_average = False, ignore_index = PAD_IDX)
                 dec_in = torch.cat((dec_in, y[:, t].unsqueeze(1)), 1) # teacher forcing
                 if VERBOSE:
                     for i, j in enumerate(dec_out.data.topk(1)[1]):
                         pred[i].append(scalar(j))
+            loss /= y.data.gt(0).sum().float() # divide by the number of unpadded tokens
             loss.backward()
             enc_optim.step()
             dec_optim.step()
