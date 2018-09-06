e8e8c5471ea800c1f874ee1a63f24fba6efa4442
threelittlemonkeys <sjlee7642@gmail.com>
2018-09-06 09:15:07 +0900

    Update by commit

--- rnn-encoder-decoder/model.py
+++ rnn-encoder-decoder/model.py
@@ -166,7 +166,7 @@ class attn(nn.Module): # attention layer (Luong et al 2015)
                 mask = mask[0]
             k = None
         a = self.align(ht, hs, mask, k) # alignment vector
-        c = a.bmm(hs) # context vector
+        c = a.bmm(hs) # context vector [B, 1, H]
         h = torch.cat((c, ht), 2)
         self.hidden = F.tanh(self.Wc(h)) # attentional vector
         return self.hidden
