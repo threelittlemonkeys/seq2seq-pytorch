45722c002420658a74ff9b75878bf93aad1f2656
threelittlemonkeys <sjlee7642@gmail.com>
2018-12-24 11:47:19 +0900

    Update by commit

--- prepare.py
+++ prepare.py
@@ -1,33 +1,30 @@
 import sys
-from model import SOS, EOS, PAD, SOS_IDX, EOS_IDX, PAD_IDX
+from model import *
 from utils import tokenize
-
-MIN_LEN = 3
-MAX_LEN = 50
 
 def load_data():
     data = []
-    src_vocab = {PAD: PAD_IDX, EOS: EOS_IDX, SOS: SOS_IDX}
-    tgt_vocab = {PAD: PAD_IDX, EOS: EOS_IDX, SOS: SOS_IDX}
+    src_vocab = {PAD: PAD_IDX, SOS: SOS_IDX, EOS: EOS_IDX, UNK: UNK_IDX}
+    tgt_vocab = {PAD: PAD_IDX, SOS: SOS_IDX, EOS: EOS_IDX, UNK: UNK_IDX}
     fo = open(sys.argv[1])
     for line in fo:
         src, tgt = line.split("\t")
-        src_tokens = tokenize(src, "word")
-        tgt_tokens = tokenize(tgt, "word")
+        src_tokens = tokenize(src, UNIT)
+        tgt_tokens = tokenize(tgt, UNIT)
         if len(src_tokens) < MIN_LEN or len(src_tokens) > MAX_LEN:
             continue
         if len(tgt_tokens) < MIN_LEN or len(tgt_tokens) > MAX_LEN:
             continue
         src_seq = []
         tgt_seq = []
-        for word in src_tokens:
-            if word not in src_vocab:
-                src_vocab[word] = len(src_vocab)
-            src_seq.append(str(src_vocab[word]))
-        for word in tgt_tokens:
-            if word not in tgt_vocab:
-                tgt_vocab[word] = len(tgt_vocab)
-            tgt_seq.append(str(tgt_vocab[word]))
+        for w in src_tokens:
+            if w not in src_vocab:
+                src_vocab[w] = len(src_vocab)
+            src_seq.append(str(src_vocab[w]))
+        for w in tgt_tokens:
+            if w not in tgt_vocab:
+                tgt_vocab[w] = len(tgt_vocab)
+            tgt_seq.append(str(tgt_vocab[w]))
         data.append((src_seq, tgt_seq))
     data.sort(key = lambda x: len(x[0]), reverse = True) # sort by source sequence length
     fo.close()
@@ -41,8 +38,8 @@ def save_data(data):
 
 def save_vocab(vocab, ext):
     fo = open(sys.argv[1] + ".vocab." + ext, "w")
-    for word, _ in sorted(vocab.items(), key = lambda x: x[1]):
-        fo.write("%s\n" % word)
+    for w, _ in sorted(vocab.items(), key = lambda x: x[1]):
+        fo.write("%s\n" % w)
     fo.close()
 
 if __name__ == "__main__":
