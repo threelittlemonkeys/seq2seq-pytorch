5aafd38ab19665fd6a92fc0ec73d76466a16ee0f
threelittlemonkeys <sjlee7642@gmail.com>
2019-09-03 11:15:55 +0900

    Update by commit

--- rnn-encoder-decoder/predict.py
+++ rnn-encoder-decoder/predict.py
@@ -10,7 +10,7 @@ def load_model():
     load_checkpoint(sys.argv[1], model)
     return model, src_vocab, tgt_vocab
 
-def greedy_search(dec, tgt_vocab, batch, eos, dec_out, heatmap):
+def greedy_search(dec, itw, batch, eos, dec_out, heatmap):
     p, dec_in = dec_out.topk(1)
     y = dec_in.view(-1).tolist()
     for i in range(len(eos)):
@@ -19,50 +19,48 @@ def greedy_search(dec, tgt_vocab, batch, eos, dec_out, heatmap):
         batch[i][3].append(y[i])
         batch[i][4] += p[i]
         eos[i] = (y[i] == EOS_IDX)
-        heatmap[i].append([tgt_vocab[y[i]]] + dec.attn.a[i][0].tolist())
+        heatmap[i].append([itw[y[i]]] + dec.attn.a[i][0].tolist())
     return dec_in
 
-def beam_search(dec, tgt_vocab, batch, t, eos, dec_out, heatmap):
-    p, y = dec_out[:len(eos)].topk(BEAM_SIZE)
-    p += Tensor([-10000 if b else a[4] for a, b in zip(batch, eos)]).unsqueeze(1)
-    p = p.view(len(eos) // BEAM_SIZE, -1)
-    y = y.view(len(eos) // BEAM_SIZE, -1)
-    if t == 0:
-        p = p[:, :BEAM_SIZE]
-        y = y[:, :BEAM_SIZE]
-    for i, (p, y) in enumerate(zip(p, y)):
+def beam_search(dec, itw, batch, t, eos, dec_out, heatmap):
+    bp, by = dec_out[:len(eos)].topk(BEAM_SIZE) # [B * BEAM_SIZE, BEAM_SIZE]
+    bp += Tensor([-10000 if b else a[4] for a, b in zip(batch, eos)]).unsqueeze(1) # update
+    bp = bp.view(-1, BEAM_SIZE ** 2) # [B, BEAM_SIZE * BEAM_SIZE]
+    by = by.view(-1, BEAM_SIZE ** 2)
+    if t == 0: # remove non-first duplicate beams
+        bp = bp[:, :BEAM_SIZE]
+        by = by[:, :BEAM_SIZE]
+    for i, (p, y) in enumerate(zip(bp, by)): # for each sequence
         j = i * BEAM_SIZE
         b1, m1 = [], [] # batch and heatmap to be updated
         if VERBOSE >= 2:
-            print("beam[%d][%d] =" % (t, i))
-            for k in range(0, len(p), BEAM_SIZE):
-                for a, b in zip(y[k:k + BEAM_SIZE], p[k:k + BEAM_SIZE]):
-                    print(((tgt_vocab[a]), round(b.item(), 4)), end = ", ")
-                print("\n")
-        for p, k in zip(*p.topk(BEAM_SIZE)):
+            for k in range(0, len(p), BEAM_SIZE): # for each beam
+                q = j + k // BEAM_SIZE
+                w = [(next(reversed(batch[q][3]), SOS_IDX), batch[q][4])] # previous token
+                w += list(zip(y, p))[k:k + BEAM_SIZE] # current candidates
+                w = [(itw[a], round(b.item(), 4)) for a, b in w]
+                print("beam[%d][%d][%d] =" % (i, t, q), w[0], "->", w[1:])
+        for p, k in zip(*p.topk(BEAM_SIZE)): # for each n-best candidate
             q = j + k // BEAM_SIZE
             b1.append(batch[q].copy())
-            b1[-1][3] = b1[-1][3] + [y[k]]
-            b1[-1][4] = p
+            b1[-1][3] = b1[-1][3] + [y[k]] # word
+            b1[-1][4] = p # probability
             m1.append(heatmap[q].copy())
-            m1[-1].append([tgt_vocab[y[k]]] + dec.attn.a[q][0][:len(batch[j][1]) + 1].tolist())
-        for k in filter(lambda x: eos[j + x], range(BEAM_SIZE)):
+            m1[-1].append([itw[y[k]]] + dec.attn.a[q][0][:len(batch[j][1]) + 1].tolist())
+        for k in filter(lambda x: eos[j + x], range(BEAM_SIZE)): # append completed sequences
             b1.append(batch[j + k])
             m1.append(heatmap[j + k])
-        if VERBOSE >= 2:
-            print("output[%d][%d] =" % (t, i))
-        x = sorted(zip(b1, m1), key = lambda x: -x[0][4])[:BEAM_SIZE]
-        for k, (a, b) in enumerate(x, j):
-            batch[k] = a
-            eos[k] = (a[3][-1] == EOS_IDX)
-            heatmap[k] = b
+        topk = sorted(zip(b1, m1), key = lambda x: -x[0][4])[:BEAM_SIZE]
+        for k, (b1, m1) in enumerate(topk, j):
+            batch[k] = b1
+            eos[k] = (b1[3][-1] == EOS_IDX)
+            heatmap[k] = m1
             if VERBOSE >= 2:
-                print([tgt_vocab[x] for x in a[3]] + [round(a[4].item(), 4)])
-        if VERBOSE >= 2:
-            print()
-    dec_in = [x[3][-1] if len(x[3]) else SOS_IDX for x in batch]
-    dec_in = LongTensor(dec_in).unsqueeze(1)
-    return dec_in
+                print("output[%d][%d][%d] = " % (i, t, k), end = "")
+                print(([itw[x] for x in b1[3]], round(b1[4].item(), 4)))
+    if VERBOSE >= 2:
+        print()
+    return LongTensor([next(reversed(x[3]), SOS_IDX) for x in batch]).unsqueeze(1)
 
 def run_model(model, tgt_vocab, batch):
     t = 0
@@ -102,7 +100,7 @@ def predict(filename, model, src_vocab, tgt_vocab):
     for idx, line in enumerate(fo):
         tkn = tokenize(line, UNIT)
         x = [src_vocab[i] if i in src_vocab else UNK_IDX for i in tkn]
-        data.extend([[idx, tkn, x, [], 0] for _ in range(BEAM_SIZE)])
+        data.extend([[idx, tkn, x, [], Tensor([0])] for _ in range(BEAM_SIZE)])
     fo.close()
     with torch.no_grad():
         model.eval()
--- utils.py
+++ utils.py
@@ -50,7 +50,7 @@ def load_checkpoint(filename, model = None):
         model.dec.load_state_dict(checkpoint["decoder_state_dict"])
     epoch = checkpoint["epoch"]
     loss = checkpoint["loss"]
-    print("saved model: epoch = %d, loss = %f" % (checkpoint["epoch"], checkpoint["loss"]))
+    print("saved model: epoch = %d, loss = %f\n" % (checkpoint["epoch"], checkpoint["loss"]))
     return epoch
 
 def save_checkpoint(filename, model, epoch, loss, time):
