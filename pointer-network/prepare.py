40fc2c4b88ea04e966745c6e16f2cfebf40aa028
threelittlemonkeys <sjlee7642@gmail.com>
2019-10-03 12:39:39 +0900

    Update by commit

--- pointer-network/prepare.py
+++ pointer-network/prepare.py
@@ -2,31 +2,33 @@ from utils import *
 
 def load_data():
     data = []
-    vocab = {PAD: PAD_IDX, SOS: SOS_IDX, EOS: EOS_IDX, UNK: UNK_IDX}
+    cti = {PAD: PAD_IDX, SOS: SOS_IDX, EOS: EOS_IDX, UNK: UNK_IDX}
+    wti = {PAD: PAD_IDX, SOS: SOS_IDX, EOS: EOS_IDX, UNK: UNK_IDX}
     fo = open(sys.argv[1])
     for line in fo:
-        src, tgt = line.split("\t")
-        src_tokens = tokenize(src, UNIT)
-        tgt_tokens = tokenize(tgt, UNIT)
-        if len(src_tokens) < MIN_LEN or len(src_tokens) > MAX_LEN:
+        x, y = line.split("\t")
+        x = tokenize(x, UNIT)
+        y = tokenize(y, UNIT)
+        if len(x) < MIN_LEN or len(x) > MAX_LEN:
             continue
-        src_seq = []
-        tgt_seq = []
-        for w in src_tokens:
-            if w not in vocab:
-                vocab[w] = len(vocab)
-            src_seq.append(str(vocab[w]))
-        for w in tgt_tokens:
-            tgt_seq.append(str(int(w) + 1))
-        tgt_seq.append(str(len(src_seq) + 1))
-        data.append((src_seq, tgt_seq))
+        for w in x:
+            for c in w:
+                if c not in cti:
+                    cti[c] = len(cti)
+            if w not in wti:
+                wti[w] = len(wti)
+        x = ["+".join(str(cti[c]) for c in w) + ":%d" % wti[w] for w in x]
+        y = [str(int(w) + 1) for w in y] + [str(len(y) + 1)]
+        data.append((x, y))
     fo.close()
     data.sort(key = lambda x: -len(x[0])) # sort by source sequence length
-    return data, vocab
+    return data, cti, wti
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit("Usage: %s training_data" % sys.argv[0])
-    data, vocab = load_data()
+    data, cti, wti = load_data()
     save_data(sys.argv[1] + ".csv", data)
-    save_vocab(sys.argv[1] + ".vocab", vocab)
+    save_tkn_to_idx(sys.argv[1] + ".char_to_idx", cti)
+    save_tkn_to_idx(sys.argv[1] + ".word_to_idx", wti)
+
