b77950973382eb947ba1c169f4b9885f8a6b47dd
threelittlemonkeys <sjlee7642@gmail.com>
2020-07-24 16:01:00 +0900

    Update by commit

--- tools/tokenizer.py
+++ tools/tokenizer.py
@@ -1,18 +1,20 @@
 import sys
 import re
 
-RE_NAN = re.compile("([^ A-Za-z0-9\u4E00-\u9FFF\uAC00-\uD7AF])") # non-alphanumeric
+RE_NALNUM = re.compile("([^ a-z0-9\u4E00-\u9FFF\uAC00-\uD7AF])", re.I)
 RE_SPACE = re.compile("[\s\u3000]+") # whitespace
-RE_APOS_L = re.compile("' (cause|d|em|ll|m|s|t|re|ve)\\b", re.IGNORECASE)
-RE_APOS_R = re.compile("\\b(goin) '", re.IGNORECASE)
+RE_APOS_L = re.compile("(^| )' (cause|d|em|ll|m|s|t|re|ve)( |$)", re.I)
+RE_APOS_R = re.compile("(^| )(goin) '( |$)", re.I)
+RE_NPTR = re.compile("(^| )(ca|could|did|does|do|had|has|have|is|was|wo|would)n 't( |$)", re.I)
 
 def tokenize(lang, filename):
     fo = open(filename)
     for line in fo:
-        line = RE_NAN.sub(" \\1 ", line)
+        line = RE_NALNUM.sub(" \\1 ", line)
         line = RE_SPACE.sub(" ", line)
-        line = RE_APOS_L.sub("'\\1", line)
-        line = RE_APOS_R.sub("\\1'", line)
+        line = RE_APOS_L.sub(r"\1'\2\3", line)
+        line = RE_APOS_R.sub(r"\1\2'\3", line)
+        line = RE_NPTR.sub(r"\1\2n't\3", line)
         line = line.strip()
         print(line)
     fo.close()
