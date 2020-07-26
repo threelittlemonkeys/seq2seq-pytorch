780a97266091f38dd72441fdf21e479c1f79d0db
threelittlemonkeys <sjlee7642@gmail.com>
2020-07-27 07:59:42 +0900

    Update by commit

--- tools/tokenizer.py
+++ tools/tokenizer.py
@@ -1,22 +1,27 @@
 import sys
 import re
 
-RE_NALNUM = re.compile("([^ a-z0-9\u4E00-\u9FFF\uAC00-\uD7AF])", re.I)
-RE_SPACE = re.compile("[\s\u3000]+") # whitespace
-RE_APOS_L = re.compile("(^| )' (cause|d|em|ll|m|s|t|re|ve)( |$)", re.I)
-RE_APOS_R = re.compile("(^| )(goin) '( |$)", re.I)
-RE_NPTR = re.compile("(^| )(ca|could|did|does|do|had|has|have|is|was|wo|would)n 't( |$)", re.I)
+RE_X_SPACE = re.compile("[\s\u3000]+") # whitespace
+RE_X_NALNUM = re.compile("([^ a-z0-9\u4E00-\u9FFF\uAC00-\uD7AF])", re.I)
+
+RE_EN_APOS_L = re.compile("(^| )' (cause|d|em|ll|m|s|t|re|ve)( |$)", re.I)
+RE_EN_APOS_R = re.compile("(^| )(goin) '( |$)", re.I)
+RE_EN_NOT = re.compile("(^| )(ca|could|did|does|do|had|has|have|is|was|wo|would)n 't( |$)", re.I)
 
 def tokenize(lang, filename):
     fo = open(filename)
     for line in fo:
-        line = RE_NALNUM.sub(" \\1 ", line)
-        line = RE_SPACE.sub(" ", line)
-        line = RE_APOS_L.sub(r"\1'\2\3", line)
-        line = RE_APOS_R.sub(r"\1\2'\3", line)
-        line = RE_NPTR.sub(r"\1\2n't\3", line)
+
+        line = RE_X_SPACE.sub(" ", line)
+        line = RE_X_NALNUM.sub(" \\1 ", line)
+
+        line = RE_EN_APOS_L.sub(r"\1'\2\3", line)
+        line = RE_EN_APOS_R.sub(r"\1\2'\3", line)
+        line = RE_EN_NOT.sub(r"\1\2n't\3", line)
+
         line = line.strip()
         print(line)
+
     fo.close()
 
 if __name__ == "__main__":
