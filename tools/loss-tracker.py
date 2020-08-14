5dc87711791afc99c54b9a5957d3b3822ff1ce1a
threelittlemonkeys <sjlee7642@gmail.com>
2020-08-14 10:52:44 +0900

    Update by commit

--- tools/loss-tracker.py
+++ tools/loss-tracker.py
@@ -5,26 +5,32 @@ def loss_diff():
     fo_data = open(sys.argv[1])
     data = fo_data.read().strip().split("\n")
     fo_data.close()
-    
+
     corpus = dict()
-    
+
     fo_idx = open(sys.argv[2])
-    fo_loss1 = open(sys.argv[3]) 
-    fo_loss2 = open(sys.argv[4]) 
-    
+    fo_loss1 = open(sys.argv[3])
+    fo_loss2 = open(sys.argv[4])
+
     for idx, loss1, loss2 in zip(fo_idx, fo_loss1, fo_loss2):
         idx = int(idx.strip())
         txt = data[idx]
         loss1 = float(loss1.strip())
         loss2 = float(loss2.strip())
         corpus[txt] = (loss1, loss2, loss2 / loss1)
-    
+
     fo_idx.close()
     fo_loss1.close()
     fo_loss2.close()
-    
+
     for seq, loss in sorted(corpus.items(), key = lambda x: -x[1][1]):
         print("%.6f\t%.6f\t%.6f\t%s" % (*loss, seq))
+
+    loss1_avrg = sum(x for x, *_ in corpus.values()) / len(corpus)
+    loss2_avrg = sum(x for _, x, _ in corpus.values()) / len(corpus)
+    ratio_avrg = sum(x for *_, x in corpus.values()) / len(corpus)
+
+    print("%.6f\t%.6f\t%.6f" % (loss1_avrg, loss2_avrg, ratio_avrg))
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
