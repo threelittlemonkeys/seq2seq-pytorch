703724c97ab72762d2616435a73f948f573517b8
threelittlemonkeys <sjlee7642@gmail.com>
2020-08-26 09:10:46 +0900

    Update by commit

--- tools/loss-filter.py
+++ tools/loss-filter.py
@@ -1,38 +1,49 @@
 import sys
 
-def load_data(corpus, data, fn_idx, fn_loss, model_idx):
+def load_data(corpus, raw_data, tkn_data, fn_idx, fn_loss, model_idx):
     fo_idx = open(fn_idx)
     fo_loss = open(fn_loss)
 
     for idx, loss in zip(fo_idx, fo_loss):
         idx = int(idx.strip())
-        txt = data[idx]
+        txt = raw_data[idx]
         loss = float(loss.strip())
         if txt not in corpus:
-            corpus[txt] = [-1, -1]
-        corpus[txt][model_idx] = loss
+            src, tgt = tkn_data[idx].split("\t")
+            src_len = src.count(" ") + 1
+            tgt_len = tgt.count(" ") + 1
+            corpus[txt] = [[-1, -1], (src_len, tgt_len)]
+        corpus[txt][0][model_idx] = loss
 
     fo_idx.close()
     fo_loss.close()
 
-def loss_tracker():
+def dcce(m1_loss, m2_loss, src_len, tgt_len): # dual conditional cross-entropy
+    if m1_loss < 0 or m2_loss < 0:
+        return -1
+    return (m1_loss + m2_loss) / 2 + abs(m1_loss - m2_loss)
+
+def loss_filter():
     corpus = dict()
 
-    fo_data = open(sys.argv[1])
-    data = fo_data.read().strip().split("\n")
-    fo_data.close()
+    with open(sys.argv[1]) as fo:
+        raw_data = fo.read().strip().split("\n")
+    with open(sys.argv[2]) as fo:
+        tkn_data = fo.read().strip().split("\n")
 
-    load_data(corpus, data, *sys.argv[2:4], 0)
-    load_data(corpus, data, *sys.argv[4:6], 1)
+    load_data(corpus, raw_data, tkn_data, *sys.argv[3:5], 0) # model 1
+    load_data(corpus, raw_data, tkn_data, *sys.argv[5:7], 1) # model 2
 
-    for txt, loss in sorted(corpus.items()):
-        score = abs(loss[0] - loss[1]) + sum(loss) / 2
+    for txt, (loss, lens) in corpus.items():
+        score = dcce(*loss, *lens)
         corpus[txt].append(score)
 
-    for txt, (*loss, score) in sorted(corpus.items(), key = lambda x: -x[1][-1]):
-        print("%.6f\t%.6f\t%.6f\t%s" % (*loss, score, txt))
+    for txt, (loss, lens, score) in sorted(corpus.items(), key = lambda x: -x[1][-1]):
+        if score < 0:
+            continue
+        print("%.6f\t%.6f\t%d\t%d\t%.6f\t%s" % (*loss, *lens, score, txt))
 
 if __name__ == "__main__":
-    if len(sys.argv) != 6:
-        sys.exit("Usage: %s m1_data m1_idx m1_loss m2_idx m2_loss" % sys.argv[0])
-    loss_tracker()
+    if len(sys.argv) != 7:
+        sys.exit("Usage: %s raw_data tkn_data m1_idx m1_loss m2_idx m2_loss" % sys.argv[0])
+    loss_filter()
